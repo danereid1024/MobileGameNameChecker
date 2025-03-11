76fae91125ca90df821e828cf574c8b378606b4b
Dane Reid <danereid@danes-mbp.mynetworksettings.com>
2025-03-10 21:15:59 -0400

    nav bar

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,38 +1,38 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 
+
 @Component({
   selector: 'app-nav',
   imports: [RouterOutlet, RouterLink],
   template: `
-
-    <nav class="navbar bg-body-tertiary fixed-top bg-primary" data-bs-theme="dark">
+<nav class="navbar bg-body-tertiary fixed-top bg-primary" data-bs-theme="dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Mobile Game Name Checker</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Mobile Game Name Checker</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-        <form class="d-flex mt-3" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-          <li class="nav-item">
-          <a class="nav-link active" aria-current="page" routerLink="valorant">Valorant</a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link active" aria-current="page" routerLink="mobile-legends">Mobile Legends</a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link active" aria-current="page" routerLink="jawaker">Jawaker</a>
-          </li>
-          <li class="nav-item"><a class="nav-link active" aria-current="page" routerLink="pubg">PUBG Mobile</a></li>
+  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Mobile Game Name Checker</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+    <form class="d-flex mt-3" role="search">
+      <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button> -->
+    </form>
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" routerLink="valorant">Valorant</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" routerLink="mobile-legends">Mobile Legends</a>
+    </li>
+<li class="nav-item">
+<a class="nav-link active" aria-current="page" routerLink="jawaker">Jawaker</a>
+</li>
+    <li class="nav-item"><a class="nav-link active" aria-current="page" routerLink="pubg">PUBG Mobile</a></li>
     <li class="nav-item"><a class="nav-link active" aria-current="page" routerLink="royal-dream">Royal Dream</a></li>
     <li class="nav-item"><a class="nav-link active" aria-current="page" routerLink="teen-patti-gold">Teen Patti Gold</a></li>
     <li class="nav-item"><a class="nav-link active" aria-current="page" routerLink="brawl-stars">Brawl Stars</a></li>
@@ -57,10 +57,10 @@ import { RouterOutlet, RouterLink } from '@angular/router';
     <li class="nav-item"><a class="nav-link active" aria-current="page" routerLink="mu-devils-awaken">MU: Devils Awaken</a></li>
     <li class="nav-item"><a class="nav-link active" aria-current="page" routerLink="metal-slug">Metal Slug</a></li>
     <li class="nav-item"><a class="nav-link active" aria-current="page" routerLink="honor-of-kings">Honor of Kings</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
+</ul>
+</div>
+</div>
+</div>
 </nav>
 
     <router-outlet></router-outlet>
@@ -74,6 +74,16 @@ export class NavComponent {
 
   constructor(private cdr: ChangeDetectorRef) { }
 
+  navItems: string[] = ['valorant', 'mobile-legends', 'jawaker', 'pubg', 'royal-dream', 'teen-patti-gold', 'brawl-stars', 'clash-royale', 'coc', 'higgs-domino', 'genshin', 'point-blank', 'arena-of-valor', 'cod-mobile', 'ace-racer', 'lifeafter', 'dragon-raja', 'eggy-party', 'hayday', 'blood-strike', 'tom-and-jerry-chase', 'speed-drifters', 'revelation', 'opm-the-strongest', 'mu-origin-3', 'mu-devils-awaken', 'metal-slug', 'honor-of-kings'];
+
+  get sortedNavItems() {
+    return this.navItems.slice().sort((a,b) => a.localeCompare(b));
+
+
+
+
+
+  }
 
   gameName = '';
 
